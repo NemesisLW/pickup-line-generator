4a5cf4a8eb8326c1fc78b8a262801543789b7f45
nemesislw <arghyadutta119@gmail.com>
2024-07-09 15:02:08 +0530

    fix: responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,53 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-pink-100">
+    <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden">
       {/* Background Image */}
-      <Image
-        src="/background.png"
-        alt="Background"
-        fill
-        className="object-cover z-0"
-        quality={100}
-        priority
-      />
-      {/* Content Overlay */}
-      <div className="relative z-10 p-4 md:p-8 text-center max-w-lg md:max-w-2xl lg:max-w-4xl lg:p-12">
-        <h1 className="text-3xl md:text-5xl lg:text-6xl  text-white mb-6">
-          Pickup Line
-          <br />
-          Generator
-        </h1>
+      <div className="absolute inset-0">
+        <Image
+          src="/background.png"
+          alt="Background"
+          fill
+          className="object-cover object-center 2xl:object-fill"
+          quality={100}
+          priority
+        />
+      </div>
 
-        <Button className="px-4 py-2 md:px-6 md:py-3 lg:px-8 lg:py-4 text-sm md:text-lg font-semibold text-white  rounded-full hover:bg-rose-600 transition duration-300">
-          <span className="flex items-center space-x-2">
-            <Image
-              src="/ph_heart-fill.svg"
-              width={16}
-              height={17}
-              alt="Heart"
-            />
-            <span>Generate one for me</span>
-            <Image
-              src="/ph_heart-fill.svg"
-              width={16}
-              height={17}
-              alt="Heart"
-            />
-          </span>
+      {/* Content Overlay */}
+      <div className="relative z-10 flex flex-col items-center justify-center w-full max-w-[90%] md:max-w-[80%] lg:max-w-[70%] text-center">
+        <div className="">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl text-white leading-tight">
+            Pickup Line
+            <br />
+            Generator
+          </h1>
+        </div>
+        <Button
+          asChild
+          className="px-4 py-2 md:px-6 md:py-3 lg:px-8 lg:py-4 text-sm md:text-lg text-white rounded-full hover:bg-rose-600 transition duration-300 shadow-xl"
+        >
+          <Link href="/generate">
+            <span className="flex items-center space-x-2">
+              <Image
+                src="/ph_heart-fill.svg"
+                width={16}
+                height={17}
+                alt="Heart"
+              />
+              <span>Generate one for me</span>
+              <Image
+                src="/ph_heart-fill.svg"
+                width={16}
+                height={17}
+                alt="Heart"
+              />
+            </span>
+          </Link>
         </Button>
       </div>
 
