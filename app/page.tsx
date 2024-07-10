be5a5eb32987b8e3d55639a65243e67d66bd6857
nemesislw <arghyadutta119@gmail.com>
2024-07-10 17:47:05 +0530

    chore: cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,8 @@ export default function Home() {
 
         {/* Content Overlay */}
         <div className="relative -left-2 z-10 flex w-full flex-col items-center justify-center gap-16 p-8 text-center md:gap-24 md:p-12 lg:-left-4 lg:-top-20 lg:gap-36 lg:p-48">
-          <div className="mb-12 lg:mb-24">
+          {/* <div className="mb-12 lg:mb-24"> */}
+          <div className="mb-12 mt-2 sm:mt-0 lg:mb-24">
             <h1 className="text-2xl leading-tight text-white md:text-3xl lg:text-5xl">
               Pickup Line
               <br />
