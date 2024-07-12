10db1a7b1522b36163a57d9efdd1237ee6a3aaf9
nemesislw <arghyadutta119@gmail.com>
2024-07-12 22:37:34 +0530

    style: experimenting with home route design for pixel-perfect execution and responsive design

--- app/alternate-landing/page.tsx
+++ app/alternate-landing/page.tsx
@@ -1,11 +1,12 @@
 import GenerateButton from "@/components/buttons/generate-cta-button";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-function page() {
+function LandingPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="absolute inset-0">
+    <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-[#FED8D8] md:bg-transparent">
+      <div className="absolute inset-0 hidden md:block">
         <Image
           src="/background.png"
           alt="Background"
@@ -15,19 +16,48 @@ function page() {
           priority
         />
       </div>
-      {/* Content Overlay */}
-      <div className="relative -left-2 z-10 flex w-full flex-col items-center justify-center gap-16 p-8 text-center md:gap-24 md:p-12 lg:-left-4 lg:-top-20 lg:gap-36 lg:p-48">
-        <div className="mb-12 mt-2 sm:mt-0 lg:mb-24">
-          <h1 className="text-2xl leading-tight text-white md:text-3xl lg:text-5xl">
+
+      {/* Mobile Design */}
+      <div className="relative w-full md:hidden">
+        <Image
+          src="/background.png"
+          alt="Background"
+          fill
+          className="rounded-3xl object-cover object-center"
+          quality={100}
+          priority
+        />
+        <div className="relative -left-2 z-10 flex w-full flex-col items-center justify-center gap-16 p-8 text-center">
+          <div className="mb-12 mt-2">
+            <h1 className="text-2xl leading-tight text-white">
+              Pickup Line
+              <br />
+              Generator
+            </h1>
+          </div>
+          <Link href="/generate">
+            <GenerateButton className="mb-24 w-[150px]" />
+          </Link>
+        </div>
+      </div>
+
+      {/* Desktop Design */}
+      <div className="hidden md:flex relative -left-2 z-10 min-h-screen flex-col items-center justify-start pt-20 w-full">
+        <div className="text-center mb-64 relative -left-1.5">
+          <h1 className="text-5xl leading-tight text-white">
             Pickup Line
             <br />
             Generator
           </h1>
         </div>
-        <GenerateButton />
+        <div className="mb-32 lg:mb-40">
+          <Link href="/generate">
+            <GenerateButton className="w-[200px] lg:w-[300px]" />
+          </Link>
+        </div>
       </div>
     </main>
   );
 }
 
-export default page;
+export default LandingPage;
